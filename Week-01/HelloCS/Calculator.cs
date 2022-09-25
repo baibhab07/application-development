493452664399c6108cbf3bcddf6c87a34fccc468
Baibhab Bhattarai <baibhab.bhattarai.2001@gmail.com>
2022-09-25 08:01:59 +0545

    Calculator App

---
+++ Week-01/HelloCS/Calculator.cs
@@ -0,0 +1,36 @@
+﻿namespace Week_01.HelloCS
+{
+    public class Calculator
+    {
+        
+        public static void Addition(int num1, int num2)
+        {
+            Console.WriteLine("Addition");
+            int sum = num1 + num2;
+            Console.WriteLine("The sum is " + sum);
+        }
+
+        public static void Subtraction(int num1, int num2)
+        {
+            Console.WriteLine("Subtraction");
+            int difference = num1 - num2;
+            Console.WriteLine("The difference is " + difference);
+
+        }
+
+        public static void Multiplication(int num1, int num2)
+        {
+            Console.WriteLine("Multiplication");
+            int product = num1 * num2;
+            Console.WriteLine("The product is " + product);
+
+        }
+        public static void Division(int num1, int num2)
+        {
+            Console.WriteLine("Division");
+            double division = num1 / num2;
+            Console.WriteLine("The result is " + division);
+
+        }
+    }
+}
--- Week-01/Program.cs
+++ Week-01/Program.cs
@@ -3,8 +3,48 @@
 
 using Week_01.HelloCS;
 
-HelloWorld hello = new HelloWorld();
-hello.ReadInput();
 
+int num1 = 0; int num2 = 0;
+
+Console.WriteLine("Console Calculator App in C#\r\n");
+
+Console.WriteLine("------------------------\n");
+
+
+Console.WriteLine("Choose options from the following list below:");
+
+Console.WriteLine("Press 1 for Addition");
+Console.WriteLine("Press 2 for Subtraction");
+Console.WriteLine("Press 3 for Multiplication");
+Console.WriteLine("Press 4 for Division");
+Console.WriteLine("Enter a number:");
+var input = Console.ReadLine();
+
+
+Console.WriteLine("Enter a number and press enter:");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter another number and press enter:");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+
+switch(input)
+{
+    case "1": 
+        Calculator.Addition(num1, num2);
+        break;
+    case "2": 
+        Calculator.Subtraction(num1, num2);
+        break;
+    case "3": 
+        Calculator.Multiplication(num1, num2);
+        break;
+    case "4": 
+        Calculator.Division(num1, num2);
+        break;
+}
+
+Console.WriteLine("Press any key to exit");
+Console.ReadKey();
 
 
